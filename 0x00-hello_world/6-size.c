a9af77db2e14583738669326a9d2f64cd4f96003
Emma Ndiko <apps.emmandiko@gmail.com>
2023-03-16 08:38:31 -0700

    Betty Style

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using sizeof to print the size of various types.
+ *
+ * Return: Always 0 (Succesful)
+*/
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
