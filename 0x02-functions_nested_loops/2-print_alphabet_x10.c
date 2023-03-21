f67ca81edf63987678655213b4960c746f29ab51
Emma Ndiko <apps.emmandiko@gmail.com>
2023-03-21 07:43:17 -0700

    Correct documentation

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,18 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet 10 times
+ * print_alphabet_x10 - function that will print the alphabet 10 times
  *
 */
 
 void print_alphabet_x10(void)
 {
-	int x, y;
+	int line, ch;
 
-	for (x = 0; x <= 9; ++x)
+	for (line = 0; line <= 9; ++line)
 	{
-		for (y = 'a';y <= 'z'; ++y)
-			_putchar(y);
+		for (ch = 'a'; ch <= 'z'; ++ch)
+			_putchar(ch);
 		_putchar('\n');
 	}
 }
