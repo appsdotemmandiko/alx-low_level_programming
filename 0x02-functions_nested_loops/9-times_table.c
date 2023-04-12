d951db05e8d2ae120474e9bb486451c0ce8fd8bd
Emma Ndiko <98321554+appsdotemmandiko@users.noreply.github.com>
2023-04-12 14:02:07 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,10 @@ void times_table(void)
 {
 	int num, mul, prod;
 
-	for (num = 0; num <= 9; ++num)
+	for (num = 0; num <= 9; num++)
 	{
-		_putchar(48);
-		for (mul = 1; mul <= 9; ++mul)
+		for (mul = 0; mul <= 9; mul++)
 		{
-			_putchar(',');
-			_putchar(' ');
 
 			prod = num * mul;
 			if (prod <= 9)
