737677b3585af4d31f912f66840e1f1b6cb870e1
Emma Ndiko <apps.emmandiko@gmail.com>
2023-04-17 09:37:46 -0700

    Uses pointer to print dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints dog given pointer
+ *
+ * @d: pointer to struct dog
+ *
+ * Return: nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			d->name = "(nil)";
+		if (d->owner == NULL)
+			d->owner = "(nil)";
+
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
+
